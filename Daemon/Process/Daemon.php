d2b659c3f715db9e7e8c27c2091c7499a4b82cc1
jhonatan <jhonatan.teixeira@gmail.com>
2014-06-02 01:57:25 -0300

    daemon libs

---
+++ Daemon/Process/Daemon.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Daemon\Process;
+
+abstract class Daemon extends Forkable
+{
+    public function run()
+    {
+        $pid = getmypid();
+
+        if ($pid != $this->getPid()) {
+            return;
+        }
+
+        while (!\Process\Forker::isDying()) {
+
+            try {
+                $this->execute();
+            } catch (Exception $exception) {
+                syslog(LOG_ERR, $exception->getMessage());
+            }
+
+            $this->iterate(1);
+        }
+
+        $this->stop();
+    }
+
+    public function stop()
+    {
+        //override me
+    }
+
+    protected function iterate($seconds)
+    {
+        sleep($seconds);
+
+        if (!gc_enabled()) {
+            gc_enable();
+        }
+
+        gc_collect_cycles();
+    }
+
+    abstract protected function execute();
+}
---
+++ Daemon/Process/Forkable.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Daemon\Process;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+
+abstract class Forkable
+{
+    private $pid;
+    
+    private $inputInterface;
+    
+    private $eventDispatcher;
+    
+    private $params = array();
+    
+    public function __construct(InputInterface $input, EventDispatcher $eventDispatcher)
+    {
+        $this->inputInterface   = $input;
+        $this->eventDispatcher  = $eventDispatcher;
+        
+        $this->validateArguments($input);
+    }
+        
+    public function getPid()
+    {
+        return $this->pid;
+    }
+
+    public function setPid($pid)
+    {
+        $this->pid = $pid;
+    }
+    
+    protected function getMandatoryArgumentMap()
+    {
+        return array();
+    }
+    
+    protected function validateArguments(InputInterface $input)
+    {
+        $mandatoryArguments = $this->getMandatoryArgumentMap();
+        $options = $input->getOption('params');
+        $options = $this->parseOptions($options);
+        
+        foreach ($mandatoryArguments as $mandatoryArgument) {
+            if (!isset($options[$mandatoryArgument])) {
+                throw new \InvalidArgumentException("$mandatoryArgument is rquired on params options");
+            }
+        }
+        
+        $this->params = $options;
+    }
+    
+    protected function parseOptions($options)
+    {
+        $parsed = array();
+
+        foreach ($options as $option) {
+            $keyValues = explode('=', $option);
+
+            for ($i=0; $i < count($keyValues)-1; $i+=2) {
+                $value = $keyValues[$i+1];
+                $explodedValue = explode(',', $value);
+                $parsed[$keyValues[$i]] = count($explodedValue) > 1 ? $explodedValue : $explodedValue[0];
+            }
+        }
+        
+        return $parsed;
+    }
+    
+    public function getParam($name, $default = null)
+    {
+        if (isset($this->params[$name])) {
+            return $this->params[$name];
+        }
+        
+        return $default;
+    }
+    
+    public function getEventDispatcher()
+    {
+        return $this->eventDispatcher;
+    }
+        
+    public abstract function run();
+}
